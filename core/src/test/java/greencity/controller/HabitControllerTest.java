fc19aae0c2b639529603498124a842678c5db98f
Demyd <demidd06@gmail.com>
2025-09-25 19:23:47 +0300

    Created imitation Current User where i released supportsParameter and resolveArgument. When i created CurrentUserHandler method in setUp i think that CurrentSecurityContextArgumentResolver equals CurrentUserHandler, because they have maybe equal logics.

--- core/src/test/java/greencity/controller/HabitControllerTest.java
+++ core/src/test/java/greencity/controller/HabitControllerTest.java
@@ -1,5 +1,7 @@
 package greencity.controller;
 
+import greencity.annotations.CurrentUser;
+import greencity.dto.user.UserVO;
 import greencity.service.HabitService;
 import greencity.service.TagsService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,10 +10,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.MethodParameter;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.security.web.method.annotation.CurrentSecurityContextArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,8 +35,28 @@ class HabitControllerTest {
     @BeforeEach
     void setUp() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(habitController)
-                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver(),
-                        new CurrentSecurityContextArgumentResolver()).build() ;
+                .setCustomArgumentResolvers(
+                        new PageableHandlerMethodArgumentResolver(),
+                        new CurrentUserHandler())
+                                      .build() ;
+    }
+
+    private static class CurrentUserHandler implements HandlerMethodArgumentResolver {
+        @Override
+        public boolean supportsParameter(MethodParameter parameter) {
+            return parameter != null
+                    && parameter.hasParameterAnnotation(CurrentUser.class)
+                    && UserVO.class.isAssignableFrom(parameter.getParameterType());
+
+        }
+
+        @Override
+        public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+            UserVO userVO = new UserVO();
+            userVO.setId(1L);
+            userVO.setEmail("test@gmail.com");
+            return userVO;
+        }
     }
 
 }
