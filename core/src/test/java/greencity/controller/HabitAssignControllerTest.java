ef6ac72cb5b3f70bded73fa1e3da5edad011b86d
Demyd <demidd06@gmail.com>
2025-09-26 14:50:17 +0300

    Init settings for tests.

---
+++ core/src/test/java/greencity/controller/HabitAssignControllerTest.java
@@ -0,0 +1,67 @@
+package greencity.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import greencity.annotations.CurrentUser;
+import greencity.dto.user.UserVO;
+import greencity.service.HabitAssignService;
+import greencity.service.HabitService;
+import greencity.service.TagsService;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.MethodParameter;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class HabitAssignControllerTest {
+    @Mock
+    private HabitAssignService habitAssignService;
+    @InjectMocks
+    private HabitAssignController habitAssignController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(habitAssignController)
+                                      .setCustomArgumentResolvers(
+                                              new PageableHandlerMethodArgumentResolver(),
+                                              new CurrentUserHandler())
+                                      .build() ;
+    }
+
+    private static class CurrentUserHandler implements HandlerMethodArgumentResolver {
+        @Override
+        public boolean supportsParameter(@NotNull MethodParameter parameter) {
+            return parameter.hasParameterAnnotation(CurrentUser.class)
+                    && UserVO.class.isAssignableFrom(parameter.getParameterType());
+
+        }
+
+        @Override
+        public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+            UserVO userVO = new UserVO();
+            userVO.setId(1L);
+            userVO.setEmail("test@gmail.com");
+            return userVO;
+        }
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
