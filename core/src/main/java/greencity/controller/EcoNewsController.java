12bbebd74c247af712ca99ebd7e228da80b1d3ee
dimoria <di.dubenok05@gmail.com>
2025-10-03 01:08:12 +0300

    Fixed [Eco-News, POST/econews/uploadImages] The response code 415 (Unsupported Media Type) is not documented in the response codes list (Bug#30)

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -108,17 +108,22 @@ public class EcoNewsController {
     @Operation(summary = "Upload array of images for eco news.")
     @ResponseStatus(value = HttpStatus.CREATED)
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = HttpStatuses.CREATED),
-        @ApiResponse(responseCode = "303", description = HttpStatuses.SEE_OTHER),
-        @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN),
+            @ApiResponse(responseCode = "201", description = HttpStatuses.CREATED),
+            @ApiResponse(responseCode = "303", description = HttpStatuses.SEE_OTHER),
+            @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN),
             @ApiResponse(responseCode = "415", description = HttpStatuses.UNSUPPORTED_MEDIA_TYPE)
     })
     @PostMapping(path = "/uploadImages", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String[]> uploadImages(
             @Parameter(description = "Array of eco news images") MultipartFile[] images) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(
-                ecoNewsService.uploadImages(images));
+        if (images == null || images.length == 0) {
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
+        }
+
+        String[] uploadedPaths = ecoNewsService.uploadImages(images);
+        return ResponseEntity.status(HttpStatus.CREATED).body(uploadedPaths);
     }
+
 
     /**
      * {@inheritDoc}
