b971e6881a563aa1375a7ec85fb725fe6f33f968
Ivan Popov <102420174+ivanyevg@users.noreply.github.com>
2025-10-03 13:21:05 +0300

    docs(econews): Document 400 Bad Request for DELETE /econews/{econewsId} Resolves: #78

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -256,9 +256,10 @@ public class EcoNewsController {
      */
     @Operation(summary = "Delete eco news.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
-        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
-        @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
     })
     @DeleteMapping("/{econewsId}")
     public ResponseEntity<Object> delete(@PathVariable Long econewsId,
