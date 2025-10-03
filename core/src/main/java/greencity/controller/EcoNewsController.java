d110cb2260990545d035792504b3fedc0cd80ae4
Ivan Popov <102420174+ivanyevg@users.noreply.github.com>
2025-10-03 14:24:58 +0300

    docs(econews): Document 415 Unsupported Media Type for POST /econews Resolves: #76

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -69,6 +69,8 @@ public class EcoNewsController {
             @ApiResponse(responseCode = "201", description = HttpStatuses.CREATED,
                     content = @Content(schema = @Schema(implementation = EcoNewsGenericDto.class))),
             @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+            @ApiResponse(responseCode = "415", description = HttpStatuses.UNSUPPORTED_MEDIA_TYPE,
                     content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
     })
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
