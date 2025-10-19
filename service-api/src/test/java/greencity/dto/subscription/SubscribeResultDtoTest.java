2e0017f335f69dfdfb6da51c9f1ab1209687d6ba
Lain <ivan.shankin04@gmail.com>
2025-10-19 20:09:19 +0300

    CI/CD. Fixed#1 format for service-api.

--- service-api/src/test/java/greencity/dto/subscription/SubscribeResultDtoTest.java
+++ service-api/src/test/java/greencity/dto/subscription/SubscribeResultDtoTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 class SubscribeResultDtoTest {
 
     @Test
     @DisplayName("Test case when subscription is successful (OK, not previously subscribed)")
     void isOkAndNewSubscription() {
         SubscribeResultDto exceptedDto = SubscribeResultDto.builder()
-                                                   .ok(true)
-                                                   .alreadySubscribed(false)
-                                                   .build();
+            .ok(true)
+            .alreadySubscribed(false)
+            .build();
 
         assertTrue(exceptedDto.isOk(), "The 'ok' field must be true for a successful operation.");
-        assertFalse(exceptedDto.isAlreadySubscribed(), "The 'alreadySubscribed' field must be false for a new subscription.");
+        assertFalse(exceptedDto.isAlreadySubscribed(),
+            "The 'alreadySubscribed' field must be false for a new subscription.");
     }
 
     @Test
     @DisplayName("Test case when email was already subscribed (OK, already subscribed)")
     void isOkButAlreadySubscribed() {
         SubscribeResultDto exceptedDto = SubscribeResultDto.builder()
-                                                   .ok(true)
-                                                   .alreadySubscribed(true)
-                                                   .build();
+            .ok(true)
+            .alreadySubscribed(true)
+            .build();
 
         assertTrue(exceptedDto.isOk(), "The 'ok' field must be true.");
         assertTrue(exceptedDto.isAlreadySubscribed(), "The 'alreadySubscribed' field must be true.");
